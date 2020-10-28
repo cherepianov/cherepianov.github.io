febe7c9cebcc0dc2e8e834c807c9e761c6af85ec
cherepianov <53432566+cherepianov@users.noreply.github.com>
2020-10-28 21:02:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-	let comment = document.getElementById("comment");
-	let howMuchLeft = document.getElementById("how-much-left");
+document.addEventListener("DOMContentLoaded", function() {
+  let comment = document.getElementById("comment");
+  let howMuchLeft = document.getElementById("how-much-left");
 
-	comment.addEventListener("input", function(event) {
-		let val = comment.value;
-		let size = val.length;
+  comment.addEventListener("input", function(event) {
+    let val = comment.value;
+    let size = val.length;
 
-		if (size >= 10) {
-			let newVal = val.slice(0, 9)
-			comment.value = newVal;
-			event.stopPropagation()
-		}
-		else {
-			console.log(size)
-		}
+    if (size >= 10) {
+      let newVal = val.slice(0, 9)
+      comment.value = newVal;
+      event.stopPropagation()
+    } else {
+      console.log(size)
+    }
 
-		howMuchLeft.innerHTML = "nabrano " + size + "/" 
-	});
-} ;
+    howMuchLeft.innerHTML = "Nabrano " + size + "/10";
+  });
+});
+
