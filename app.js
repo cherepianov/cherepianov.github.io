e0dca5c5c7cba216d5c2105bf762348f59332254
cherepianov <53432566+cherepianov@users.noreply.github.com>
2020-10-28 21:00:43 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+	let comment = document.getElementById("comment");
+	let howMuchLeft = document.getElementById("how-much-left");
+
+	comment.addEventListener("input", function(event) {
+		let val = comment.value;
+		let size = val.length;
+
+		if (size >= 10) {
+			let newVal = val.slice(0, 9)
+			comment.value = newVal;
+			event.stopPropagation()
+		}
+		else {
+			console.log(size)
+		}
+
+		howMuchLeft.innerHTML = "nabrano " + size + "/" 
+	});
+} ;
